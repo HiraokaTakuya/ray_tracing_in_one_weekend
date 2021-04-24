968b9f4ef6cc3de571e5f7eee7c286d1fa23c2ca
HiraokaTakuya <hiraoka64@gmail.com>
2021-04-24 22:49:23 +0900

    Change Vec3<T> to Vec3. Remove the generic parameter.

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,37 +1,12 @@
-use std::marker::PhantomData;
-
-#[derive(Clone)]
-pub struct PointType;
-#[derive(Clone)]
-pub struct DirectionType;
-#[derive(Clone)]
-pub struct ColorType;
-
-pub trait Vec3Trait {}
-
-impl Vec3Trait for PointType {}
-impl Vec3Trait for DirectionType {}
-impl Vec3Trait for ColorType {}
-
-#[derive(Debug, Clone)]
-pub struct Vec3<T>
-where
-    T: Vec3Trait,
-{
+#[derive(Debug, Clone, Copy)]
+pub struct Vec3 {
     e: [f64; 3],
-    _marker: PhantomData<fn() -> T>,
 }
 
 #[allow(dead_code)]
-impl<T> Vec3<T>
-where
-    T: Vec3Trait,
-{
+impl Vec3 {
     pub fn new(e0: f64, e1: f64, e2: f64) -> Self {
-        Self {
-            e: [e0, e1, e2],
-            _marker: PhantomData,
-        }
+        Self { e: [e0, e1, e2] }
     }
     pub fn length_squared(&self) -> f64 {
         self[0] * self[0] + self[1] * self[1] + self[2] * self[2]
@@ -51,76 +26,55 @@ where
     }
 }
 
-impl<T> std::ops::Neg for Vec3<T>
-where
-    T: Vec3Trait,
-{
-    type Output = Vec3<T>;
+impl std::ops::Neg for Vec3 {
+    type Output = Vec3;
 
     fn neg(self) -> Self::Output {
-        Vec3::<T>::new(-self[0], -self[1], -self[2])
+        Vec3::new(-self[0], -self[1], -self[2])
     }
 }
 
-impl<T> std::ops::Add for Vec3<T>
-where
-    T: Vec3Trait,
-{
-    type Output = Vec3<T>;
+impl std::ops::Add for Vec3 {
+    type Output = Vec3;
 
-    fn add(self, other: Vec3<T>) -> Self::Output {
-        Vec3::<T>::new(self[0] + other[0], self[1] + other[1], self[2] + other[2])
+    fn add(self, other: Vec3) -> Self::Output {
+        Vec3::new(self[0] + other[0], self[1] + other[1], self[2] + other[2])
     }
 }
 
-impl<T> std::ops::AddAssign for Vec3<T>
-where
-    T: Vec3Trait,
-{
-    fn add_assign(&mut self, rhs: Vec3<T>) {
+impl std::ops::AddAssign for Vec3 {
+    fn add_assign(&mut self, rhs: Vec3) {
         self[0] += rhs[0];
         self[1] += rhs[1];
         self[2] += rhs[2];
     }
 }
 
-impl<T> std::ops::Sub for Vec3<T>
-where
-    T: Vec3Trait,
-{
-    type Output = Vec3<T>;
+impl std::ops::Sub for Vec3 {
+    type Output = Vec3;
 
-    fn sub(self, other: Vec3<T>) -> Self::Output {
-        Vec3::<T>::new(self[0] - other[0], self[1] - other[1], self[2] - other[2])
+    fn sub(self, other: Vec3) -> Self::Output {
+        Vec3::new(self[0] - other[0], self[1] - other[1], self[2] - other[2])
     }
 }
 
-impl<T> std::ops::SubAssign for Vec3<T>
-where
-    T: Vec3Trait,
-{
-    fn sub_assign(&mut self, rhs: Vec3<T>) {
+impl std::ops::SubAssign for Vec3 {
+    fn sub_assign(&mut self, rhs: Vec3) {
         self[0] -= rhs[0];
         self[1] -= rhs[1];
         self[2] -= rhs[2];
     }
 }
 
-impl<T> std::ops::Mul<f64> for Vec3<T>
-where
-    T: Vec3Trait,
-{
-    type Output = Vec3<T>;
+impl std::ops::Mul<f64> for Vec3 {
+    type Output = Vec3;
 
     fn mul(self, rhs: f64) -> Self::Output {
-        Vec3::<T>::new(self[0] * rhs, self[1] * rhs, self[2] * rhs)
+        Vec3::new(self[0] * rhs, self[1] * rhs, self[2] * rhs)
     }
 }
 
-impl<T> std::ops::MulAssign<f64> for Vec3<T>
-where
-    T: Vec3Trait,
-{
+impl std::ops::MulAssign<f64> for Vec3 {
     fn mul_assign(&mut self, rhs: f64) {
         self[0] *= rhs;
         self[1] *= rhs;
@@ -128,32 +82,23 @@ where
     }
 }
 
-impl<T> std::ops::Mul<Vec3<T>> for f64
-where
-    T: Vec3Trait,
-{
-    type Output = Vec3<T>;
+impl std::ops::Mul<Vec3> for f64 {
+    type Output = Vec3;
 
-    fn mul(self, rhs: Vec3<T>) -> Self::Output {
+    fn mul(self, rhs: Vec3) -> Self::Output {
         rhs * self
     }
 }
 
-impl<T> std::ops::Div<f64> for Vec3<T>
-where
-    T: Vec3Trait,
-{
-    type Output = Vec3<T>;
+impl std::ops::Div<f64> for Vec3 {
+    type Output = Vec3;
 
     fn div(self, rhs: f64) -> Self::Output {
-        Vec3::<T>::new(self[0] / rhs, self[1] / rhs, self[2] / rhs)
+        Vec3::new(self[0] / rhs, self[1] / rhs, self[2] / rhs)
     }
 }
 
-impl<T> std::ops::DivAssign<f64> for Vec3<T>
-where
-    T: Vec3Trait,
-{
+impl std::ops::DivAssign<f64> for Vec3 {
     fn div_assign(&mut self, rhs: f64) {
         self[0] /= rhs;
         self[1] /= rhs;
@@ -161,10 +106,7 @@ where
     }
 }
 
-impl<T> std::ops::Index<usize> for Vec3<T>
-where
-    T: Vec3Trait,
-{
+impl std::ops::Index<usize> for Vec3 {
     type Output = f64;
 
     fn index(&self, index: usize) -> &Self::Output {
@@ -172,19 +114,13 @@ where
     }
 }
 
-impl<T> std::ops::IndexMut<usize> for Vec3<T>
-where
-    T: Vec3Trait,
-{
+impl std::ops::IndexMut<usize> for Vec3 {
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
         &mut self.e[index]
     }
 }
 
-impl<T> std::fmt::Display for Vec3<T>
-where
-    T: Vec3Trait,
-{
+impl std::fmt::Display for Vec3 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
@@ -197,210 +133,134 @@ where
 }
 
 #[allow(dead_code)]
-pub type Point = Vec3<PointType>;
+pub type Point = Vec3;
 #[allow(dead_code)]
-pub type Direction = Vec3<DirectionType>;
+pub type Direction = Vec3;
 #[allow(dead_code)]
-pub type Color = Vec3<ColorType>;
-
-impl std::ops::Add<Direction> for Point {
-    type Output = Point;
-
-    fn add(self, other: Direction) -> Self::Output {
-        Point::new(self[0] + other[0], self[1] + other[1], self[2] + other[2])
-    }
-}
-
-impl std::ops::AddAssign<Direction> for Point {
-    fn add_assign(&mut self, rhs: Direction) {
-        self[0] += rhs[0];
-        self[1] += rhs[1];
-        self[2] += rhs[2];
-    }
-}
+pub type Color = Vec3;
 
 #[test]
 fn test_length() {
-    fn f<T: Vec3Trait>() {
-        let e = [10.0, 20.0, 30.0];
-        let v = Vec3::<T>::new(e[0], e[1], e[2]);
-        let ls = v.length_squared();
-        let ls_ans = (e[0] * e[0] + e[1] * e[1] + e[2] * e[2]) as i32;
-        assert_eq!(ls_ans, 1400);
-        assert_eq!(ls.round() as i32, ls_ans);
-        let l = v.length();
-        let l_ans: i32 = f64::from(ls_ans).sqrt().round() as i32;
-        assert_eq!(l_ans, 37);
-        assert_eq!(l.round() as i32, l_ans);
-    }
-    f::<PointType>();
-    f::<DirectionType>();
-    f::<ColorType>();
+    let e = [10.0, 20.0, 30.0];
+    let v = Vec3::new(e[0], e[1], e[2]);
+    let ls = v.length_squared();
+    let ls_ans = (e[0] * e[0] + e[1] * e[1] + e[2] * e[2]) as i32;
+    assert_eq!(ls_ans, 1400);
+    assert_eq!(ls.round() as i32, ls_ans);
+    let l = v.length();
+    let l_ans: i32 = f64::from(ls_ans).sqrt().round() as i32;
+    assert_eq!(l_ans, 37);
+    assert_eq!(l.round() as i32, l_ans);
 }
 
 #[test]
 fn test_neg() {
-    fn f<T: Vec3Trait>() {
-        let l = Vec3::<T>::new(10.0, 20.0, 30.0);
-        let v = -l;
-        assert_eq!(v[0].round() as i64, -10);
-        assert_eq!(v[1].round() as i64, -20);
-        assert_eq!(v[2].round() as i64, -30);
-        let v = -v;
-        assert_eq!(v[0].round() as i64, 10);
-        assert_eq!(v[1].round() as i64, 20);
-        assert_eq!(v[2].round() as i64, 30);
-    }
-    f::<PointType>();
-    f::<DirectionType>();
-    f::<ColorType>();
+    let l = Vec3::new(10.0, 20.0, 30.0);
+    let v = -l;
+    assert_eq!(v[0].round() as i64, -10);
+    assert_eq!(v[1].round() as i64, -20);
+    assert_eq!(v[2].round() as i64, -30);
+    let v = -v;
+    assert_eq!(v[0].round() as i64, 10);
+    assert_eq!(v[1].round() as i64, 20);
+    assert_eq!(v[2].round() as i64, 30);
 }
 
 #[test]
 fn test_add() {
-    fn f<T: Vec3Trait>() {
-        let l = Vec3::<T>::new(10.0, 20.0, 30.0);
-        let r = Vec3::<T>::new(10.0, 20.0, 30.0);
-        let v = l + r;
-        assert_eq!(v[0].round() as i64, 20);
-        assert_eq!(v[1].round() as i64, 40);
-        assert_eq!(v[2].round() as i64, 60);
-    }
-    f::<PointType>();
-    f::<DirectionType>();
-    f::<ColorType>();
+    let l = Vec3::new(10.0, 20.0, 30.0);
+    let r = Vec3::new(10.0, 20.0, 30.0);
+    let v = l + r;
+    assert_eq!(v[0].round() as i64, 20);
+    assert_eq!(v[1].round() as i64, 40);
+    assert_eq!(v[2].round() as i64, 60);
 }
 
 #[test]
 fn test_add_assign() {
-    fn f<T: Vec3Trait>() {
-        let mut l = Vec3::<T>::new(10.0, 20.0, 30.0);
-        let r = Vec3::<T>::new(10.0, 20.0, 30.0);
-        l += r;
-        assert_eq!(l[0].round() as i64, 20);
-        assert_eq!(l[1].round() as i64, 40);
-        assert_eq!(l[2].round() as i64, 60);
-    }
-    f::<PointType>();
-    f::<DirectionType>();
-    f::<ColorType>();
+    let mut l = Vec3::new(10.0, 20.0, 30.0);
+    let r = Vec3::new(10.0, 20.0, 30.0);
+    l += r;
+    assert_eq!(l[0].round() as i64, 20);
+    assert_eq!(l[1].round() as i64, 40);
+    assert_eq!(l[2].round() as i64, 60);
 }
 
 #[test]
 fn test_sub() {
-    fn f<T: Vec3Trait>() {
-        let l = Vec3::<T>::new(10.0, 20.0, 30.0);
-        let r = Vec3::<T>::new(5.0, 10.0, 15.0);
-        let v = l - r;
-        assert_eq!(v[0].round() as i64, 5);
-        assert_eq!(v[1].round() as i64, 10);
-        assert_eq!(v[2].round() as i64, 15);
-    }
-    f::<PointType>();
-    f::<DirectionType>();
-    f::<ColorType>();
+    let l = Vec3::new(10.0, 20.0, 30.0);
+    let r = Vec3::new(5.0, 10.0, 15.0);
+    let v = l - r;
+    assert_eq!(v[0].round() as i64, 5);
+    assert_eq!(v[1].round() as i64, 10);
+    assert_eq!(v[2].round() as i64, 15);
 }
 
 #[test]
 fn test_sub_assign() {
-    fn f<T: Vec3Trait>() {
-        let mut l = Vec3::<T>::new(10.0, 20.0, 30.0);
-        let r = Vec3::<T>::new(5.0, 10.0, 15.0);
-        l -= r;
-        assert_eq!(l[0].round() as i64, 5);
-        assert_eq!(l[1].round() as i64, 10);
-        assert_eq!(l[2].round() as i64, 15);
-    }
-    f::<PointType>();
-    f::<DirectionType>();
-    f::<ColorType>();
+    let mut l = Vec3::new(10.0, 20.0, 30.0);
+    let r = Vec3::new(5.0, 10.0, 15.0);
+    l -= r;
+    assert_eq!(l[0].round() as i64, 5);
+    assert_eq!(l[1].round() as i64, 10);
+    assert_eq!(l[2].round() as i64, 15);
 }
 
 #[test]
 fn test_mul() {
-    fn f<T: Vec3Trait>() {
-        let l = Vec3::<T>::new(10.0, 20.0, 30.0);
-        let r = 3.0;
-        let v = l * r;
-        assert_eq!(v[0].round() as i64, 30);
-        assert_eq!(v[1].round() as i64, 60);
-        assert_eq!(v[2].round() as i64, 90);
-    }
-    f::<PointType>();
-    f::<DirectionType>();
-    f::<ColorType>();
+    let l = Vec3::new(10.0, 20.0, 30.0);
+    let r = 3.0;
+    let v = l * r;
+    assert_eq!(v[0].round() as i64, 30);
+    assert_eq!(v[1].round() as i64, 60);
+    assert_eq!(v[2].round() as i64, 90);
 }
 
 #[test]
 fn test_mul_assign() {
-    fn f<T: Vec3Trait>() {
-        let mut l = Vec3::<T>::new(10.0, 20.0, 30.0);
-        let r = 3.0;
-        l *= r;
-        assert_eq!(l[0].round() as i64, 30);
-        assert_eq!(l[1].round() as i64, 60);
-        assert_eq!(l[2].round() as i64, 90);
-    }
-    f::<PointType>();
-    f::<DirectionType>();
-    f::<ColorType>();
+    let mut l = Vec3::new(10.0, 20.0, 30.0);
+    let r = 3.0;
+    l *= r;
+    assert_eq!(l[0].round() as i64, 30);
+    assert_eq!(l[1].round() as i64, 60);
+    assert_eq!(l[2].round() as i64, 90);
 }
 
 #[test]
 fn test_div() {
-    fn f<T: Vec3Trait>() {
-        let l = Vec3::<T>::new(10.0, 20.0, 30.0);
-        let r = 2.0;
-        let v = l / r;
-        assert_eq!(v[0].round() as i64, 5);
-        assert_eq!(v[1].round() as i64, 10);
-        assert_eq!(v[2].round() as i64, 15);
-    }
-    f::<PointType>();
-    f::<DirectionType>();
-    f::<ColorType>();
+    let l = Vec3::new(10.0, 20.0, 30.0);
+    let r = 2.0;
+    let v = l / r;
+    assert_eq!(v[0].round() as i64, 5);
+    assert_eq!(v[1].round() as i64, 10);
+    assert_eq!(v[2].round() as i64, 15);
 }
 
 #[test]
 fn test_div_assign() {
-    fn f<T: Vec3Trait>() {
-        let mut l = Vec3::<T>::new(10.0, 20.0, 30.0);
-        let r = 2.0;
-        l /= r;
-        assert_eq!(l[0].round() as i64, 5);
-        assert_eq!(l[1].round() as i64, 10);
-        assert_eq!(l[2].round() as i64, 15);
-    }
-    f::<PointType>();
-    f::<DirectionType>();
-    f::<ColorType>();
+    let mut l = Vec3::new(10.0, 20.0, 30.0);
+    let r = 2.0;
+    l /= r;
+    assert_eq!(l[0].round() as i64, 5);
+    assert_eq!(l[1].round() as i64, 10);
+    assert_eq!(l[2].round() as i64, 15);
 }
 
 #[test]
 fn test_index() {
-    fn f<T: Vec3Trait>() {
-        let l = Vec3::<T>::new(10.0, 20.0, 30.0);
-        assert_eq!(l[0].round() as i64, 10);
-        assert_eq!(l[1].round() as i64, 20);
-        assert_eq!(l[2].round() as i64, 30);
-    }
-    f::<PointType>();
-    f::<DirectionType>();
-    f::<ColorType>();
+    let l = Vec3::new(10.0, 20.0, 30.0);
+    assert_eq!(l[0].round() as i64, 10);
+    assert_eq!(l[1].round() as i64, 20);
+    assert_eq!(l[2].round() as i64, 30);
 }
 
 #[test]
 fn test_index_mut() {
-    fn f<T: Vec3Trait>() {
-        let mut l = Vec3::<T>::new(10.0, 20.0, 30.0);
-        l[0] = 5.0;
-        l[1] = 10.0;
-        l[2] = 15.0;
-        assert_eq!(l[0].round() as i64, 5);
-        assert_eq!(l[1].round() as i64, 10);
-        assert_eq!(l[2].round() as i64, 15);
-    }
-    f::<PointType>();
-    f::<DirectionType>();
-    f::<ColorType>();
+    let mut l = Vec3::new(10.0, 20.0, 30.0);
+    l[0] = 5.0;
+    l[1] = 10.0;
+    l[2] = 15.0;
+    assert_eq!(l[0].round() as i64, 5);
+    assert_eq!(l[1].round() as i64, 10);
+    assert_eq!(l[2].round() as i64, 15);
 }
