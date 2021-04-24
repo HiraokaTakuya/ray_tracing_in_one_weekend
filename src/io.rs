c89c5465a3ff386e5ab8ce8187c684fab85dfc1f
HiraokaTakuya <hiraoka64@gmail.com>
2021-04-24 23:35:12 +0900

    Refactoring for the first PPM.

--- src/io.rs
+++ src/io.rs
@@ -1,3 +1,6 @@
+use crate::ray::Ray;
+use crate::vec3::{Color, Point, Vec3};
+
 #[allow(dead_code)]
 pub fn process() {
     const IMAGE_WIDTH: usize = 256;
@@ -7,15 +10,12 @@ pub fn process() {
     println!("P3\n{} {}\n{}", IMAGE_WIDTH, IMAGE_HEIGHT, MAX_BRIGHTNESS);
     for h in (0..IMAGE_HEIGHT).rev() {
         for w in 0..IMAGE_WIDTH {
-            let r = w as f64 / (IMAGE_WIDTH - 1) as f64;
-            let g = h as f64 / (IMAGE_HEIGHT - 1) as f64;
-            let b = 0.25;
-
-            let r = (255.999 * r) as i32;
-            let g = (255.999 * g) as i32;
-            let b = (255.999 * b) as i32;
-
-            println!("{} {} {}", r, g, b);
+            let color = Color::new(
+                w as f64 / (IMAGE_WIDTH - 1) as f64,
+                h as f64 / (IMAGE_HEIGHT - 1) as f64,
+                0.25,
+            );
+            println!("{}", color);
         }
     }
 }
