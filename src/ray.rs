8011d97e9fb50b451db35d5a320d3783439e5994
HiraokaTakuya <hiraoka64@gmail.com>
2021-04-26 00:14:04 +0900

    Adding a sphere.

--- src/ray.rs
+++ src/ray.rs
@@ -14,8 +14,20 @@ impl Ray {
         self.origin + self.direction * t
     }
     pub fn color(&self) -> Color {
-        let unit_direction = self.direction.clone().unit();
+        if hit_sphere(&Point::new(0.0, 0.0, -1.0), 0.5, &self) {
+            return Color::new(1.0, 0.0, 0.0);
+        }
+        let unit_direction = self.direction.unit();
         let t = (unit_direction[1] + 1.0) * 0.5;
         (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
     }
+}
+
+fn hit_sphere(center: &Point, radius: f64, r: &Ray) -> bool {
+    let oc = r.origin - *center;
+    let a = r.direction.dot(&r.direction);
+    let b = 2.0 * oc.dot(&r.direction);
+    let c = oc.dot(&oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    discriminant > 0.0
 }
