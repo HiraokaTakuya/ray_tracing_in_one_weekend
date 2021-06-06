fadce9fb41c1ce2306409335eec058f9eef7f7dc
HiraokaTakuya <hiraoka64@gmail.com>
2021-06-07 01:48:09 +0900

    Fix index.html, index.js

--- www/index.js
+++ www/index.js
@@ -1,3 +1,18 @@
-import * as wasm from "hello-wasm-pack";
+'use strict'
 
-wasm.greet();
+console.log("start loading wasm");
+const ray_tracing = import('../pkg')
+    .catch(console.error);
+Promise.all([ray_tracing]).then(async function ([{ draw_ray_tracing_set }]) {
+    console.log("finished loading wasm");
+
+    const renderBtn = document.getElementById('render');
+    renderBtn.addEventListener('click', () => {
+        let jsResult = null;
+        let wasmResult = null;
+        {
+            console.log("wasm only");
+            draw_ray_tracing_set();
+        }
+    });
+});
