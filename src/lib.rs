1c07736a7e59f4770c8e42f1003e206140ac987f
HiraokaTakuya <hiraoka64@gmail.com>
2021-06-07 00:42:47 +0900

    Use wasm-pack.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,70 @@
 mod camera;
 mod hittable;
 mod hittable_list;
-pub mod io;
+mod io;
 mod material;
 mod ray;
 mod sphere;
 mod vec3;
+
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::{Clamped, JsCast};
+use web_sys::ImageData;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(a: &str);
+}
+
+macro_rules! console_log {
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+
+macro_rules! measure_elapsed_time {
+    ($t:tt,$s:block) => {{
+        let window = web_sys::window().expect("should have a window in this context");
+        let performance = window
+            .performance()
+            .expect("performance should be available");
+        let start = performance.now();
+        let result = { $s };
+        let end = performance.now();
+        console_log!("{}:{}[ms]", $t, end - start);
+        result
+    }};
+}
+
+#[wasm_bindgen]
+pub fn draw_ray_tracing_set() {
+    const CANVAS_ID: &str = "canvas_wasm";
+    let document = web_sys::window().unwrap().document().unwrap();
+    let canvas = document.get_element_by_id(CANVAS_ID).unwrap();
+    let canvas: web_sys::HtmlCanvasElement = canvas
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .map_err(|_| ())
+        .unwrap();
+    let context = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
+
+    let mut result = measure_elapsed_time!("\tgenerate:wasm\telapsed:", { io::process() });
+    measure_elapsed_time!("\tdraw:wasm\telapsed:", {
+        let data = ImageData::new_with_u8_clamped_array_and_sh(
+            Clamped(&mut result),
+            canvas.width(),
+            canvas.height(),
+        );
+        if let Ok(data) = data {
+            let _ = context.put_image_data(&data, 0.0, 0.0);
+        }
+    })
+}
